675d9d1fe417207244a11ab4fc495969713a221c
Luke-png <72192057+Luke-png@users.noreply.github.com>
2021-12-10 15:21:49 +0100

    l

--- Othello/Othello.hs
+++ Othello/Othello.hs
@@ -8,6 +8,7 @@ import Data.Maybe
 -- 8x8 , 32 brickor vadare , pattern in the begining, can only place so that a color changes , 
 
 data Color = White | Black
+ deriving ( Show, Eq )
 opposite White = Black
 opposite Black = White
 
@@ -57,19 +58,30 @@ boardSizeValid :: Board -> Bool
 boardSizeValid b = (length r) == 8 && all (==8) (map length r)
   where r = rows b
 
+-- All of the boards positions
+blanks :: Board -> [Pos]
+blanks (Board rows) = [ (n, k) | n <- [0..7], k <- [0..7], rows !! n !! k == Nothing ]
+
+nonBlanks :: Board -> [Pos]
+nonBlanks (Board rows) = [ (n, k) | n <- [0..7], k <- [0..7], rows !! n !! k /= Nothing ]
+
+
+getCell :: Board -> Pos -> Cell
+getCell (Board rows) (r,c) = ((rows!!r)!!c)
+
+
 -- Returns all possible placements for a new piece, depending on the color. 
 --validPlacements :: Color -> [Pos]
 
--- Returns all the empty cells surrounding the opposite color.
-borderPos :: Board -> Color -> [Pos]
-borderPos b c = 
+-- Returns the positions of all the cells of one color.
+colorCells :: Board -> Color -> [Pos]
+colorCells b c = filter color $ nonBlanks b
+  where color pos = (getCell b pos) == (Just c)
 
 -- Get a list of the surrounding cells.
 surrounding :: Pos -> [(Pos)]
 surrounding (r,c) = filter validPos square
   where square = [(x,y) | x <- [r-1..r+1], y <- [c-1..c+1]]\\[(r,c)]
-
-validPos :: (Pos) -> Bool
-validPos (r,c) = elem r [0..8] && elem c [0..8]
+        validPos (a,b) = elem a [0..8] && elem b [0..8]
 
 
