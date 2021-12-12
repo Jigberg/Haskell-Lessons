5cb5cd4cd5f39608f5ac1ba806875a9f292603a1
Luke-png <72192057+Luke-png@users.noreply.github.com>
2021-12-12 16:29:43 +0100

    asd

--- Othello/Othello.hs
+++ Othello/Othello.hs
@@ -25,7 +25,7 @@ startBoard :: Board
 startBoard = 
     Board
     [ [n  ,n  ,n  ,n  ,n  ,n  ,n  ,n  ]
-    , [n  ,n  ,n  ,n  ,n  ,n  ,n  ,n  ]
+    , [n  ,n  ,j w,n  ,n  ,n  ,n  ,n  ]
     , [n  ,n  ,n  ,n  ,n  ,n  ,n  ,n  ]
     , [n  ,n  ,n  ,j b,j w,n  ,n  ,n  ]
     , [n  ,n  ,n  ,j w,j b,n  ,n  ,n  ]
@@ -73,13 +73,53 @@ getCell (Board rows) (r,c) = ((rows!!r)!!c)
 -- Returns all possible placements for a new piece, depending on the color. 
 --validPlacements :: Color -> [Pos]
 
+
+-- Returns all the places where a new colored piece can be placed. 
+validPlacements :: Board -> Color -> [Pos]
+validPlacements b c = filter canFlip $ semiValid b c
+  where canFlip pos = flipCheck b c pos
+
+-------------
+
+-- Checks if the placement can flip any pieces.
+flipCheck :: Board -> Color -> Pos -> Bool
+flipCheck b c pos = horizontal b c pos -- && vertical b c pos && diagonal b c pos
+
+-- Checks if a possible placement can flip any pieces horizontaly.
+horizontal :: Board -> Color -> Pos -> Bool
+horizontal b c (r,col)
+ | getCell b (r,(col-1)) == Just (opposite c) && (col-2)>(-1) = lineCheck b c $ reverse $ filter color [(r,x) | x <- [0..col]]
+ | getCell b (r,(col+1)) == Just (opposite c) && (col+2)<(8) = lineCheck b c $ filter color [(r,x) | x <- [col..7]]
+ | otherwise = False
+  where color pos = (getCell b pos) == (Just c)
+
+-- Checks if a hor/ver/dia line from position x can result in a flip. 
+lineCheck :: Board -> Color -> [Pos] -> Bool
+lineCheck b c [] = False
+lineCheck b c (x:xs)
+ | getCell b x == (Just c) = True
+ | getCell b x == Just (opposite c) = lineCheck b c xs
+ | otherwise = False
+
+-------------
+
+-- Returns all semi possible placements, includes the ones that doesn't flip a the opponents pieces. 
+semiValid :: Board -> Color -> [Pos]
+semiValid b c = filter empty (semiValid' b c $ colorCells b c)
+   where empty pos = (getCell b pos) == Nothing
+
+semiValid' :: Board -> Color -> [Pos] -> [Pos]
+semiValid' b c []     = []
+semiValid' b c (x:xs) = surrounding x ++ semiValid' b c xs
+
+
 -- Returns the positions of all the cells of one color.
 colorCells :: Board -> Color -> [Pos]
 colorCells b c = filter color $ nonBlanks b
   where color pos = (getCell b pos) == (Just c)
 
 -- Get a list of the surrounding cells.
-surrounding :: Pos -> [(Pos)]
+surrounding :: Pos -> [Pos]
 surrounding (r,c) = filter validPos square
   where square = [(x,y) | x <- [r-1..r+1], y <- [c-1..c+1]]\\[(r,c)]
         validPos (a,b) = elem a [0..8] && elem b [0..8]
